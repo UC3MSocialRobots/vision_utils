338a0e03aa3d25a71a34274ee21019b73605837f
Arnaud Ramey <arnaud.a.ramey@gmail.com>
2015-03-08 10:11:03 +0000

    compiling with hydro

--- src/image_utils/content_processing.h
+++ src/image_utils/content_processing.h
@@ -360,6 +360,60 @@ inline void nonNulPoints(const cv::Mat1b & img, Pt2Iterable & rep) {
 #endif
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+/*! Find the non null point with the lowest row index
+ * in a mask, among non zero pixels.
+ * We only search in columns with an index close to the vertical center
+ * of the bounding box of the content.
+ * \arg width_ratio_to_bbox in [0, 1]
+ *   around 0, very narrow search window
+ *   around 1, very wide search window (almost equal to image bbox)
+ * \return (-1, -1) if not found
+ */
+template<class _T>
+cv::Point find_top_point_centered(const cv::Mat_<_T> & image,
+                                  bool compute_bbox,
+                                  const double width_ratio_to_bbox = .3,
+                                  const _T zero_value = _T(0),
+                                  cv::Rect* search_window_out = NULL) {
+  assert(width_ratio_to_bbox > 0 && width_ratio_to_bbox <= 1);
+  // find bounding box and allowed columns
+  cv::Rect bbox = (compute_bbox ? image_utils::boundingBox(image) : bbox_full(image));
+  int
+      min_col = bbox.x + (1.f - width_ratio_to_bbox) * bbox.width / 2,
+      max_col = bbox.x + (1.f + width_ratio_to_bbox) * bbox.width / 2,
+      min_row = bbox.y,
+      max_row = bbox.y + bbox.height;
+  //min_col = std::max(min_col, 0);
+  //max_col = std::min(max_col, image.cols - 1);
+  // return search window if user curious about it
+  if (search_window_out != NULL)
+    *search_window_out = cv::Rect(min_col, min_row, max_col - min_col + 1, max_row - min_row + 1);
+
+  // run among the allowed window
+  for (int row = min_row; row < max_row; ++row) {
+    // get the address of row
+    const _T* data = image.ptr(row);
+    for (int col_begin = min_col; col_begin <= max_col; ++col_begin) {
+      if (data[col_begin] != zero_value) {
+        // now see where the non zeros stop
+        int col_end = col_begin;
+        while (col_end < image.cols && data[col_end] != zero_value)
+          ++col_end;
+        // return the middle of these non zero values
+        return cv::Point((col_begin + col_end) / 2, row);
+      }
+    } // end loop col_begin
+  } // end loop row
+  printf("find_top_point_centered(img:%ix%i, search_window:(%ix%i)->(%ix%i)): "
+         "could not find a non zero point!\n",
+         image.cols, image.rows, min_col, min_row, max_col, max_row);
+  return cv::Point(-1, -1);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 /*!
  *\brief   get a vector from an image with all the non nul points
  */
@@ -1311,58 +1365,6 @@ private:
   CMatrix<bool> floodfill_buffer;
 }; // end class FloodFillEdgeCloser
 
-
-////////////////////////////////////////////////////////////////////////////////
-
-/*! Find the non null point with the lowest row index
- * in a mask, among non zero pixels.
- * We only search in columns with an index close to the vertical center
- * of the bounding box of the content.
- * \arg width_ratio_to_bbox in [0, 1]
- *   around 0, very narrow search window
- *   around 1, very wide search window (almost equal to image bbox)
- * \return (-1, -1) if not found
- */
-template<class _T>
-cv::Point find_top_point_centered(const cv::Mat_<_T> & image,
-                                  bool compute_bbox,
-                                  const double width_ratio_to_bbox = .3,
-                                  const _T zero_value = _T(0),
-                                  cv::Rect* search_window_out = NULL) {
-  assert(width_ratio_to_bbox > 0 && width_ratio_to_bbox <= 1);
-  // find bounding box and allowed columns
-  cv::Rect bbox = (compute_bbox ? image_utils::boundingBox(image) : bbox_full(image));
-  int
-      min_col = bbox.x + (1.f - width_ratio_to_bbox) * bbox.width / 2,
-      max_col = bbox.x + (1.f + width_ratio_to_bbox) * bbox.width / 2,
-      min_row = bbox.y,
-      max_row = bbox.y + bbox.height;
-  //min_col = std::max(min_col, 0);
-  //max_col = std::min(max_col, image.cols - 1);
-  // return search window if user curious about it
-  if (search_window_out != NULL)
-    *search_window_out = cv::Rect(min_col, min_row, max_col - min_col + 1, max_row - min_row + 1);
-
-  // run among the allowed window
-  for (int row = min_row; row < max_row; ++row) {
-    // get the address of row
-    const _T* data = image.ptr(row);
-    for (int col_begin = min_col; col_begin <= max_col; ++col_begin) {
-      if (data[col_begin] != zero_value) {
-        // now see where the non zeros stop
-        int col_end = col_begin;
-        while (col_end < image.cols && data[col_end] != zero_value)
-          ++col_end;
-        // return the middle of these non zero values
-        return cv::Point((col_begin + col_end) / 2, row);
-      }
-    } // end loop col_begin
-  } // end loop row
-  printf("find_top_point_centered(img:%ix%i, search_window:(%ix%i)->(%ix%i)): "
-         "could not find a non zero point!\n",
-         image.cols, image.rows, min_col, min_row, max_col, max_row);
-  return cv::Point(-1, -1);
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 
